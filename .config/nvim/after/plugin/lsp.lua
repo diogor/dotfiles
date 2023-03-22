82f20e0c4d78758baa78a1bbc76e17fad7f5a67e
Diogo Lima <diogo@dr6.com.br>
2023-03-22 17:00:17 -0300

    disable TAB for cmp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,13 @@
 local lsp = require('lsp-zero')
+local cmp = require('cmp')
 
 lsp.preset('recommended')
+
+lsp.setup_nvim_cmp({
+  mapping = lsp.defaults.cmp_mappings({
+    ['<Tab>'] = vim.NIL,
+  })
+})
+
 lsp.setup()
 

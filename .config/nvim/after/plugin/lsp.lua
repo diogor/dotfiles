da6c6e3345c0eeadb8a7e6777c6e4291465f4fa0
Diogo Lima <diogo@dr6.com.br>
2024-01-03 16:52:44 -0300

    disable tab for cmp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -14,11 +14,18 @@ for _, lsp in pairs(servers) do
     }
 end
 
-lsp.setup()
 
-lsp.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({
-    buffer = bufnr,
-    exclude = {'<Tab>'},
-  })
-end)
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+  ['<Tab>'] = vim.NIL,
+  ['<S-Tab>'] = vim.NIL
+})
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+lsp.setup()

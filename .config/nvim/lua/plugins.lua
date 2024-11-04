fd2d944ac8335961f01faa2144dd0bdc01c5dbaa
Diogo Lima <diogo@dr6.com.br>
2024-11-03 22:32:46 -0300

    waybar locale, nvim markdown

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -50,6 +50,15 @@ local plugins = {
     { 'LunarVim/breadcrumbs.nvim', dependencies = { 'SmiteshP/nvim-navic' } },
     'nvim-pack/nvim-spectre',
     { 'Exafunction/codeium.vim', event = 'BufEnter' },
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    }
 }
 local opts = {}
 

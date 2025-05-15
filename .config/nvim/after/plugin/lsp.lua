24aa8afcae71201782578f56d35e1ed50c6fba49
Diogo Lima <diogo@dr6.com.br>
2025-05-15 18:31:09 -0300

    fix nvim conf

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,57 +1,26 @@
-local lsp = require('lsp-zero')
+local lsp_zero = require('lsp-zero')
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
-local mason_lspconfig = require("mason-lspconfig")
 local lspconfig = require('lspconfig')
 
-lsp.preset('recommended')
-
 local servers = { 'pyright', 'ts_ls', 'jdtls', 'gopls' }
-for _, lsp in pairs(servers) do
-    lspconfig[lsp].setup {
-        on_attach = on_attach,
-        flags = {
-          debounce_text_changes = 150,
-        }
-    }
-end
 
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
 
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-  ['<Tab>'] = vim.NIL,
-  ['<S-Tab>'] = vim.NIL
+lsp_zero.setup_servers(servers)
+
+cmp.setup({
+  mapping = {
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-n>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-p>'] = cmp_action.luasnip_jump_backward(),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
 })
-
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
-lsp.setup({
-capabilities = {
-      workspace = {
-        didChangeWatchedFiles = {
-          dynamicRegistration = true,
-        },
-      },
-    },
-})
-
-mason_lspconfig.setup_handlers({
-    ["jdtls"] = function()
-        lspconfig.jdtls.setup {
-            cmd = {
-                "jdtls",
-                "--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.expand "$MASON/share/jdtls/lombok.jar"),
-            },
-            capabilities = capabilities,
-            handlers = {
-                ["language/status"] = progress_handler,
-            },
-        }
-    end
-})
-
